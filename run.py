92c9620829678c0731370d8cdf0d5f02ea2844f8
fyh <2271470941@qq.com>
2022-10-20 21:37:02 +0800

    update to flask

--- run.py
+++ run.py
@@ -1,15 +1,8 @@
 # 创建应用实例
 import sys
 
-from flask import Flask, render_template, request, redirect, url_for, flash, session
-
-
 from wxcloudrun import app
 
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
 # 启动Flask Web服务
 if __name__ == '__main__':
     app.run(host=sys.argv[1], port=sys.argv[2])
