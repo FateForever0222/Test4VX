d7ab3030b20a5d05a1c841e272e508853c092535
fyh <2271470941@qq.com>
2022-10-21 16:04:00 +0800

    get openid

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,9 +1,17 @@
 from flask import render_template, request
 from run import app
 
+@app.before_request
+def before_request():
+    ip = request.remote_addr
+    url = request.url
+    print(ip)
+    print(url)
 
 @app.route('/')
 
 def login():
+    user_wx_openid=request.headers.get('x-wx-openid')
+    print(user_wx_openid)
     return render_template('login.html')
 
