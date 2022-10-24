45fdfd815263a8b35411a4e9df6f07c1e93b9454
fyh <2271470941@qq.com>
2022-10-24 15:26:13 +0800

    verify2

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,16 +1,13 @@
 from flask import render_template, request
 from run import app
 
-@app.before_request
-def before_request():
-    ip = request.remote_addr
-    url = request.url
-    print(ip)
-    print(url)
-
 @app.route('/')
 
 def login():
     user_wx_openid=request.headers.get('x-wx-openid')
     return render_template('login.html',openid=user_wx_openid)
 
+@app.route("/MP_verify_PertlLt3DIqUIQJv.txt")
+
+def verify():
+    return "PertlLt3DIqUIQJv"
